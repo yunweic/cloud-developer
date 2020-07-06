1a8dc22e3274d9a3d45a1e56d95eac6e792bc649
Steven Chang <stevechang@graphen.ai>
2020-07-05 22:24:45 -0400

    works for lesson 4

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,68 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id',  
+    async (req: Request, res: Response) => {
+        
+        let { id } = req.params;
+
+        if ( !id ) {
+            return res.status(400)
+                    .send(`id is required`);
+        }
+
+        const item = await FeedItem.findByPk(id);
+        
+        //respond not found, if we do not have this id
+        if(!item){
+            return res.status(404).send(`feed item is not found`);
+        }
+
+        res.send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        let { id } = req.params;
+
+        if ( !id ) {
+            return res.status(400)
+                    .send(`id is required`);
+        }
+
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+
+        // check Caption is valid
+        if (!caption) {
+            return res.status(400).send({ message: 'Caption is required or malformed' });
+        }
+
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({ message: 'File url is required' });
+        }
+
+        const item = await FeedItem.findByPk(id);
+        
+        //respond not found, if we do not have this id
+        if(!item){
+            return res.status(404).send(`feed item is not found`);
+        }
+
+        item.url = fileName;
+        item.caption = caption;
+
+        const saved_item = await item.save();
+
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
+
+
+        // res.send(500).send("not implemented")
 });
 
 
